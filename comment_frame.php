b36baf1cb0543287b72e140c2c3595926f5f9450
Andrija Buzinac <andrija.buzinac@gmail.com>
2018-08-09 00:39:44 +0200

    social network project

--- comment_frame.php
+++ comment_frame.php
@@ -9,6 +9,13 @@
 </head>
 <body>
 
+<style type="text/css">
+    * {
+        font-size: 12px;
+        font-family: Bellota-LightItalic, sans-serif;
+    }
+</style>
+
 <?php
 
 //todo we can change in to include
@@ -54,7 +61,7 @@ $posted_to = $row['added_by'];
 if (isset($_POST['postComment' . $post_id])) {
     $post_body = $_POST['post_body'];
     $post_body = mysqli_escape_string($con, $post_body);
-    $date_time_now = $date("Y-m-d H:i:s"); // todo change date format
+    $date_time_now = date("Y-m-d H:i:s"); // todo change date format
     $insert_post = mysqli_query($con, "INSERT INTO post_comment VALUES ('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id')");
     echo "<p>Comment Posted! </p>";
 }
@@ -68,6 +75,97 @@ if (isset($_POST['postComment' . $post_id])) {
 </form>
 
 <!-- Load Comments -->
+
+<?php
+
+$get_comments = mysqli_query($con, "SELECT * FROM post_comment WHERE post_id='$post_id' ORDER BY id DESC ");
+$count = mysqli_num_rows($get_comments);
+
+if ($count != 0) {
+    while ($comment = mysqli_fetch_array($get_comments)) {
+
+        $comment_body = $comment['post_body'];
+        $posted_to = $comment['posted_to'];
+        $posted_by = $comment['posted_by'];
+        $date_added = $comment['date_added'];
+        $removed = $comment['removed'];
+
+        //Time frame
+        $date_time_now = date("Y-m-d H:i:s"); //todo change date format
+        $start_date = new Datetime($date_added);  // time of post
+        $end_date = new DateTime($date_time_now);// current time
+        $interval = $start_date->diff($end_date);// differences between dates
+        if ($interval->y >= 1) {
+            if ($interval == 1)
+                $time_message = $interval->y . " year ago"; // 1 year ago
+            else
+                $time_message = $interval->y . " years ago"; // 1+ year ago
+        } elseif ($interval->m >= 1) {
+            if ($interval->d == 0) {
+                $days = " ago";
+            } elseif ($interval->d == 1) {
+                $days = $interval->d . " day ago";
+            } else {
+                $days = $interval->d . " days ago";
+            }
+
+            if ($interval->m == 1) {
+                $time_message = $interval->m . " month" . $days;
+            } else {
+                $time_message = $interval->m . " months" . $days;
+            }
+        } elseif ($interval->d >= 1) {
+            if ($interval->d == 1) {
+                $time_message = "Yesterday";
+            } else {
+                $time_message = $interval->d . " days ago";
+            }
+        } elseif ($interval->h >= 1) {
+            if ($interval->h == 1) {
+                $time_message = $interval->h . " hour ago";
+            } else {
+                $time_message = $interval->h . " hours ago";
+            }
+        } elseif ($interval->i >= 1) {
+            if ($interval->i == 1) {
+                $time_message = $interval->i . " minute ago";
+            } else {
+                $time_message = $interval->i . " minutes ago";
+            }
+        } else {
+            if ($interval->s < 30) {
+                $time_message = "Just now";
+            } else {
+                $time_message = $interval->s . " seconds ago";
+            }
+        }
+
+        $user_obj = new User($con, $posted_by);
+
+        ?>
+
+        <div class="comment_section">
+            <a href="<?php echo $posted_by; ?>" target="_parent"><img src="<?php echo $user_obj->getProfilePic(); ?>"
+                                                                      alt="profile_pic"
+                                                                      title="<?php echo $posted_by; ?>"
+                                                                      style="float: left"
+                                                                      height="30"></a>
+            <a href="<?php echo $posted_by; ?>" target="_parent"><b><?php echo $user_obj->getFirstAndLastName(); ?></b></a>
+            &nbsp;&nbsp;&nbsp;&nbsp; <?php echo $time_message . "<br>" . $comment_body; ?>
+            <hr>
+        </div>
+
+        <?php
+
+    }
+}
+
+else {
+    echo "<center><br><br>No Comments to Show!</center>";
+}
+
+?>
+
 
 </body>
 </html>
